645ba7038cfcbc3bce9ef8a9e4acf19c6d9a9b50
JEFFERSON ANDRES MORENO PEDRAZA <jamp9504@gmail.com>
2024-10-27 05:21:08 -0500

    consumo coffee y cats, pendiente consumir cualquier otra en un componente aparte

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+// import ApiCoffeeCarruselBasic  from './components/ApiCoffeeCarruselBasic';
+import { CatGalleryFetch } from './components/CatGalleryFetch';
+
+function App() {
+
+
+  return (
+    <div>
+      {/* <ApiCoffeeCarruselBasic /> */}
+      <CatGalleryFetch />
+
+    </div>
+  )
+}
+
+export default App
